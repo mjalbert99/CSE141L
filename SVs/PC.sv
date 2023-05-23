fde1d08536dcd43889b7da84dcbaec8efc8660fa
mjalbert99 <67979651+mjalbert99@users.noreply.github.com>
2023-05-23 16:24:02 -0700

    formatting

--- SVs/PC.sv
+++ SVs/PC.sv
@@ -5,14 +5,13 @@ module PC #(parameter D=10)(
   input reset,					// synchronous reset
         clk,
         absjump_en,				// abs. jump enable
-  input       [D-1:0] target,	// how far/where to jump
-  output logic[D-1:0] prog_ctr
+        input       [D-1:0] target,	// how far/where to jump
+        output logic[D-1:0] prog_ctr
 );
 
   always_ff @(posedge clk)
   if(reset)
 	  prog_ctr <= '0;
-
   else if(absjump_en)
 	  prog_ctr <= target;
 	else
