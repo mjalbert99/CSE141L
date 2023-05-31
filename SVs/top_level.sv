9971f0e307b76224acf293257e7d726a6ba4cf21
mjalbert99 <mjalbert@ucsd.edu>
2023-05-30 17:28:17 -0700

    updated to have reg controller

--- SVs/top_level.sv
+++ SVs/top_level.sv
@@ -90,19 +90,6 @@ module top_level(
       .immediate  
     );	
 
-  // assign rd_addrA = mach_code[1:0];
-  // assign immediate = RegDst ? rd_addrA : mach_code[3:0]; // 4 bit or 2 it immediate
-  // assign addrA = MemtoReg ? mach_code[3:2] : rd_addrA;  // Handles loads
-  // assign str_addrA = MemWrite ? mach_code[3:2] : addrA;  // Handles stores
-  // assign inA = Branch ? mach_code[3:2] : str_addrA;   // Handles branching
-  
-  // assign rd_addrB = mach_code[3:2];
-  // assign AddrB = RegDst ? rd_addrB : mach_code[5:4]; // 4 bit or 2 it immediate
-  // assign addrB = MemtoReg ? mach_code[5:4] : AddrB;  // Handles loads
-  // assign str_addrB = MemWrite ? mach_code[5:4] : addrB;  // Handles stores
-  // assign inB = Branch ? mach_code[5:4] : str_addrB;   // Handles branching
-
-  //assign wr_addr = mach_code[5:4];
 
   reg_file #(.pw(3)) rf1(
               .dat_in(regDataIn),	   // loads, most ops 
